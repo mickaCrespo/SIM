1417a757077fc0109acd9614e9320fce6f93bfa3
mickaCrespo <30704383+mickaCrespo@users.noreply.github.com>
2018-04-02 14:20:48 -0300

    Update GeneradorMultiplicativoMixto.java

--- src/GeneradorMultiplicativoMixto.java
+++ src/GeneradorMultiplicativoMixto.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
